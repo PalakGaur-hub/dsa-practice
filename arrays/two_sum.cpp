a45ee658f2e4d11c835971b7716ebb8aa7e090c0
PalakGaur-hub <p30669422@gmail.com>
2025-12-21 20:36:02 +0530

    Add Two Sum solution in C++

---
+++ arrays/two_sum.cpp
@@ -0,0 +1,12 @@
+// Problem: Two Sum
+// Platform: LeetCode
+// Difficulty: Easy
+// Language: C++
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
