7def5b6974d190f60006884ae730c42a9f8f17d9
PalakGaur-hub <p30669422@gmail.com>
2026-03-11 21:48:25 +0530

    Update binary_tree_inorder.cpp

--- trees/binary_tree_inorder.cpp
+++ trees/binary_tree_inorder.cpp
@@ -1,1 +1,27 @@
+// Problem: Binary Tree Inorder Traversal
+// Platform: LeetCode
+// Topic: Trees
+// Approach: Recursion (Left → Root → Right)
+// Time Complexity: O(n)
+// Space Complexity: O(h)
 
+
+ struct TreeNode {
+ int val;
+ struct TreeNode *left;
+ struct TreeNode *right;
+};
+int* inorderTraversal(struct TreeNode* root, int* returnSize) { 
+    int* result = (int*)malloc(sizeof(int) * 1000);
+    *returnSize = 0;
+    void inorder(struct TreeNode* root) {
+        if (root == NULL)
+            return;
+        inorder(root->left);                       
+        result[(*returnSize)++] = root->val;        
+        inorder(root->right);                      
+    }
+    inorder(root);
+    return result;
+}
+ LINK- https://leetcode.com/problems/binary-tree-inorder-traversal/?envType=problem-list-v2&envId=tree
